8f8f038d1434f73949aade0bce4d42c24d061b14
Mesoma Esonwune <mesonwune@gmail.com>
2019-05-06 11:56:44 -0700

    First Draft of Game - Game Class - Gameplay Class - MapGenerator Class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,16 @@
+import javax.swing.*;
+
 public class Game
 {
+    public static void main(String[] args)
+    {
+        JFrame object = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        object.setBounds(10, 10, 700, 600);
+        object.setTitle("Super Rainbow Reef!!!!!");
+        object.setResizable(false);
+        object.add(gamePlay);
+        object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        object.setVisible(true);
+    }
 }
